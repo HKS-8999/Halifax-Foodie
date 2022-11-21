490d42755887dff99d60cb3a8b6d9a86a7f940a9
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-11-20 23:41:44 -0400

    update recipe price added to the restaurant function

--- lambda/online-support-module/restaurantFunction.py
+++ lambda/online-support-module/restaurantFunction.py
@@ -8,13 +8,13 @@ def lambda_handler(event, context):
     
   
     reqEvent = event['interpretations'][0]['intent']['name']
+    recipeTableName = 'recipeTable'
     
     if(reqEvent == 'recipeIntent'):
         recipeNameFromLex = event['interpretations'][0]['intent']['slots']['recipeName']['value']['originalValue']
         recipePriceFromLex = event['interpretations'][0]['intent']['slots']['recipePrice']['value']['originalValue']
         print(recipeNameFromLex)
 
-        recipeTableName = 'recipeTable'
         recipeId = str(uuid.uuid4())
         table = boto3.resource('dynamodb').Table(recipeTableName)
         createRecipeItem = table.put_item(
@@ -42,7 +42,61 @@ def lambda_handler(event, context):
                     }
                 ]
         }
+    elif(reqEvent == 'updateRecipePriceIntent'):
+        updateRecipeNameFromLex = event['interpretations'][0]['intent']['slots']['recipeName']['value']['originalValue']
+        updateRecipePriceFromLex = event['interpretations'][0]['intent']['slots']['updatedRecipePrice']['value']['originalValue']
         
+        data = client.get_item(
+        TableName=recipeTableName,
+        Key={
+            'recipName': {'S': recipeNameFromLex}
+        }
+        )
+        if 'Item' not in data:
+            print("null")
+            return{
+              "sessionState": {
+                  "dialogAction": {
+                      "type": "Close"
+                  },
+                  "intent": {
+                      "name": reqEvent,
+                        "state": "Fulfilled"
+                  }
+              },
+              "messages": [
+                  {
+                  "contentType": "PlainText",
+                  "content": "Your restaurant inventory does not have any recipe with name " + updateRecipeNameFromLex + "."
+                  }
+                ]
+            }
+        else:
+            table = boto3.resource('dynamodb').Table(recipeTableName)
+            recipeIdFromDynamo = data['Item']['recipeId']['S']
+            print(recipeId)
+            updateRecipePriceRes = table.update_item(
+                Key={'recipeId': recipeIdFromDynamo},
+                UpdateExpression="SET recipePrice = :newRecipePrice",
+                ExpressionAttributeValues={":newRecipePrice": updateRecipePriceFromLex},
+            )
+            return {
+                  "sessionState": {
+                      "dialogAction": {
+                          "type": "Close"
+                      },
+                      "intent": {
+                          "name": reqEvent,
+                            "state": "Fulfilled"
+                      }
+                  },
+                  "messages": [
+                      {
+                      "contentType": "PlainText",
+                      "content": "The price for the recipe " + updateRecipeNameFromLex+" with recipe Id " + recipeIdFromDynamo+ " has been updated to " + updateRecipePriceFromLex + "."
+                      }
+                    ]
+                }   
     else:
       orderIdFromLex = event['interpretations'][0]['intent']['slots']['orderId']['value']['originalValue']
       print(orderIdFromLex)
