8f73c20ec753ab15fd17c221bf93e673605d80df
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-12-01 17:42:29 -0400

    changes in landing page

--- client/restaurant/src/components/AuthenticatedApp/index.jsx
+++ client/restaurant/src/components/AuthenticatedApp/index.jsx
@@ -3,7 +3,6 @@ import { Landing } from "../Landing";
 import { ChatRoom } from "../ChatRoom";
 import { SessionList } from "../SessionList";
 import { RestaurantChat } from "../RestaurantChat";
-import Home from "../Home";
 import Feedback from "../Polarity";
 import UploadImageToS3WithNativeSdk from "../DataProcessing/DataProcessing";
 
@@ -11,11 +10,10 @@ function AuthenticatedApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Landing />} />
         <Route path="/restaurant/chat/" element={<RestaurantChat />} />
         <Route path="/restaurant/chat/:id" element={<ChatRoom />} />
         <Route path="/restaurant/chat/:id/:id" element={<SessionList />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/customerFeedbackPolarity" element={<Feedback />} />
         <Route
           path="/uploadRecipe"
