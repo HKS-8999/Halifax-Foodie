fe1ef78398acdec7cadf0c79985980757be59664
Harsh Shah <hr838264@dal.ca>
2022-12-04 18:44:25 -0400

    Resolved merge conflicts

--- client/user/src/components/Landing/index.jsx
+++ client/user/src/components/Landing/index.jsx
@@ -5,12 +5,8 @@ import React, { Component } from 'react';
 function Landing() {
   return (
     <div>
-<<<<<<< HEAD
       <ul className="chat-room-list">
         <h2>Welcome!</h2>
-=======
-       <ul className="chat-room-list">
->>>>>>> db7d77f9178ddef49cbc8a21c3c3a444548eeec0
         <li>
           <Link to="/user/customerFeedback">Give Feedback</Link>
         </li>
