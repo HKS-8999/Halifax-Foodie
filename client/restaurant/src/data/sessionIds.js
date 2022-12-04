6e5e54abe0fb1d0f556efba884ed65d7d907051a
Hemanth Nadipineni <54321hemanth@gmail.com>
2022-12-04 17:24:40 -0400

    changes in end session

--- client/restaurant/src/data/sessionIds.js
+++ client/restaurant/src/data/sessionIds.js
@@ -1,6 +1,14 @@
 const sessionId = [
-  { id: "10eea881-0686-4e16-96dc-abf69fc07c6e", roomId: "delivery", title: "10eea881-0686-4e16-96dc-abf69fc07c6e" },
-  { id: "bib", roomId: "payment", title: "bib" },
+  {
+    id: "10eea881-0686-4e16-96dc-abf69fc07c6e",
+    roomId: "delivery",
+    title: "10eea881-0686-4e16-96dc-abf69fc07c6e",
+  },
+  {
+    id: "2c559099-5d5d-445a-a56a-4f6b041fed0d",
+    roomId: "delivery",
+    title: "2c559099-5d5d-445a-a56a-4f6b041fed0d",
+  },
 ];
 
 export { sessionId };
